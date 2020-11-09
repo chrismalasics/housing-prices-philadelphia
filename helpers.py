cd8f0de372d46172606026ff4812bc0e03e3e510
Chris Malasics <chrismalasics@gmail.com>
2020-11-09 15:27:20 -0700

    create file to free up space in notebook

---
+++ helpers.py
@@ -0,0 +1,21 @@
+#request proxy list
+def get_proxy_list():
+    from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+    proxies = []
+    req_proxy = RequestProxy() #you may get different number of proxy when  you run this at each time
+    proxies = req_proxy.get_proxy_list() #this will create proxy list
+
+    proxies_address=[]
+    for i in proxies:
+        proxy = i.get_address()
+        proxies_address.append(proxy)
+
+    return list(set(proxies_address))
+
+def format_proxy(ip_address):
+    proxies = {'http': ip_address,
+               'https': ip_address}
+    return proxies
+
+    
+     
---
+++ settings.py
@@ -0,0 +1,14 @@
+#settings
+
+#headers
+headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+           "Accept-Encoding": "gzip, deflate, sdch, br",
+           "Accept-Language": "en-US,en;q=0.8",
+           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+          }
+
+#threads
+threads = 50
+
+
+
