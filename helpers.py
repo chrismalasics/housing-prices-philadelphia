592e187d8fc9471dac7b693847b6b5b4a75b3cdf
Chris Malasics <chrismalasics@gmail.com>
2020-11-09 20:28:19 -0700

    add functions

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-#helper functions
+#helpers for scraping
 
 def get_proxy_list():
     from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
@@ -18,5 +18,46 @@ def format_proxy(ip_address):
                'https': ip_address}
     return proxies
 
+
+#********************    
+#helpers for cleaning
+#********************
+
+#string to integer
+def str_to_int(string):
+    try:
+        int(string)
+        return int(string)
+    except:
+        return np.nan
     
-     
+#list to string
+def list_to_str(x):
+    x = x[2:-2]
+    if len(x) > 0:
+        return x
+    else:
+        return np.nan
+    
+#string to year   
+def str_to_year(x):
+    try:
+        x = x[-4:]
+        x = int(x)
+        return x
+    except:
+        return np.nan
+    
+#zero to numpy NaN
+def zero_to_nan(x):
+    if x == 0:
+        x == np.nan
+        return x
+    else:
+        return x
+
+
+
+
+
+
